54787f687a3503a5841f323e9492a4808f313d9a
nelfimov <nelfimov@mail.ru>
2022-12-29 08:20:23 +0300

    added footer and navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <header>
+      <span>LOGO</span>
+      <div className="nav-links">
+        <a href="">1 link</a>
+        <a href="">2 link</a>
+        <a href="">3 link</a>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
